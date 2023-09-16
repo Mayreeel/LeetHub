51794d5d9dad7b7edff2f256dbf492c3d1f5f758
Kyu-hyun Lee <kuwon15@gmail.com>
2023-09-16 20:16:45 +0900

    Time: 100 ms (13.73%), Space: 52.6 MB (37.75%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    let arr = [...nums];
+    k = k % nums.length
+    for(let i = 0;i<nums.length-k;i++){
+        nums[k+i] = arr[i];
+    }
+    console.log(arr)
+    console.log(nums)
+    for(let i = 0;i<k;i++){
+        nums[i] = arr[arr.length-k+i]
+    }
+};
