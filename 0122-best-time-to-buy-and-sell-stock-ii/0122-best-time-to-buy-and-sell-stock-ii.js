4c8c103710a4d3a5ea049bec124733386af5fde0
Kyu-hyun Lee <kuwon15@gmail.com>
2023-09-16 21:26:58 +0900

    Time: 47 ms (92.25%), Space: 42.5 MB (30.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -2,33 +2,13 @@
  * @param {number[]} prices
  * @return {number}
  */
-function calProfit(prices, left, right, current_profit) {
-    while (right < prices.length) {
-        if (prices[left] < prices[right]) {
-                current_profit += prices[right] - prices[left];
-                left = right;
-        } else {
-            left = right;
-        }
-    right++;
-    }
-    return current_profit;
-}
-
 var maxProfit = function(prices) {
-    let left = 0; // Buy
-    let right = 0; // sell
     let max_profit = 0;
-    let current_profit = 0;
 
-    for(let i = 0; i<prices.length-1; i++){
-        left = 0 + i;
-        right = 1 + i;
-        current_profit = calProfit(prices, left, right, current_profit)
-        if(current_profit > max_profit) {
-            max_profit = current_profit
+    for (let i = 0; i < prices.length - 1; i++) {
+        if (prices[i] < prices[i + 1]) {
+            max_profit += prices[i + 1] - prices[i];
         }
-        current_profit = 0
     }
 
     return max_profit;
