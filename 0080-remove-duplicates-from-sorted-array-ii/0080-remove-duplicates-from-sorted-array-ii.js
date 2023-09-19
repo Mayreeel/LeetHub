6f6da15960aa5fe0286ba584c67603bed85499f4
Kyu-hyun Lee <kuwon15@gmail.com>
2023-09-19 23:25:58 +0900

    Time: 65 ms (38.81%), Space: 44.7 MB (11.12%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.js
@@ -4,13 +4,13 @@
  */
 var removeDuplicates = function(nums) {
     let arr = [];
-    
+
     for(let i = 0;i<nums.length;i++){
-        if (arr.filter(x=>x==nums[i]).length >= 2){
-            nums.splice(i,1)
+        if (arr.filter(x=>x==nums[i]).length>=2){
+            nums.splice(i, 1)
             i--
         } else {
             arr.push(nums[i])
         }
     }
-};
+};  
