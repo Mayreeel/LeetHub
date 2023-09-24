8f6e197a8db744c8d2402fac4235e30097307389
Kyu-hyun Lee <kuwon15@gmail.com>
2023-09-24 17:27:53 +0900

    Time: 90 ms (6.37%), Space: 47.1 MB (5.12%) - LeetHub

---
+++ 0274-h-index/0274-h-index.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} citations
+ * @return {number}
+ */
+var hIndex = function(citations) {
+    let answer = 0;
+    for (let i = 0;i<citations.length;i++){
+        if (citations[i]>answer){
+            answer = Math.max(answer ,Math.min(citations.filter(x=>x>=citations[i]).length, citations[i]))
+        }
+    }
+    return answer
+};
