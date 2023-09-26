81205f9461e30f6d76852b665bf1c2b8a9a6609e
Kyu-hyun Lee <kuwon15@gmail.com>
2023-09-26 21:17:06 +0900

    Time: 80 ms (7.57%), Space: 47.4 MB (5.16%) - LeetHub

--- 0274-h-index/0274-h-index.js
+++ 0274-h-index/0274-h-index.js
@@ -4,8 +4,8 @@
  */
 var hIndex = function(citations) {
     let answer = 0;
-    for (let i = 0;i<citations.length;i++){
-        if (citations[i]>answer){
+    for(let i = 0; i<citations.length;i++){
+        if(citations[i]>answer){
             let currentHIndex = Math.min(citations.filter(x=>x>=citations[i]).length, citations[i])
             answer = Math.max(answer, currentHIndex)
         }
