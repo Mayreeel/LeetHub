3b02ab1bfd6758f1ba9b01e9b865cda85be3b11e
Kyu-hyun Lee <kuwon15@gmail.com>
2023-10-30 11:33:31 +0900

    Time: 82 ms (32.51%), Space: 48.6 MB (14.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,19 +4,7 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    const arr1 = s.split('')
-    const arr2 = t.split('')
-    arr1.forEach((x)=>{
-        if(arr2.length===0){
-            arr2.splice(0, 0, ' ')
-        }
-        let index = arr2.indexOf(x)
-        if (index!=-1) {
-            arr2.splice(index, 1)
-        }
-    })
-    if (arr2.length===0) {
-        return true;
-    }
-    return false;
+    const arr1 = s.split('').sort()
+    const arr2 = t.split('').sort()
+    return arr1.join()===arr2.join()? true : false
 };
