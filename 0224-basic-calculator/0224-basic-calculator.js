744ea594d88a76df1d16835fed76cb1f2dab6de3
Kyu-hyun Lee <kuwon15@gmail.com>
2023-11-07 23:35:34 +0900

    Time: 60 ms (84.76%), Space: 45 MB (59.21%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.js
+++ 0224-basic-calculator/0224-basic-calculator.js
@@ -3,22 +3,33 @@
  * @return {number}
  */
 var calculate = function(s) {
-        let res = 0, sum = 0, sign = 1;
-    let myStack = [];
-    myStack.push(1);
-    const isDigit = (ch) => {
-        return ch >= '0' && ch <= '9';
-    }
-    for(let ch of s){
-        if(isDigit(ch)) sum = sum * 10 + (ch - '0');
-        else{
-            res += sum * sign * myStack[myStack.length - 1];
-            sum = 0;
-            if(ch === '-') sign = -1;
-            else if(ch === '+') sign = 1;
-            else if(ch === '(') {myStack.push(myStack[myStack.length - 1] * sign); sign = 1;}
-            else if(ch === ')') myStack.pop(); 
+    let sign = 1, sum = 0;
+
+    const stack = [];
+    for (let i = 0; i<s.length; i +=1){if (s[i] >= '0' && s[i] <= '9') {
+        let num = 0
+        while (s[i] >= '0' && s[i] <= '9') {
+            num = (num * 10) + (s[i] - '0');
+            i += 1
         }
-    }
-    return res += (sign * sum);
+    sum += (num * sign);
+    i -= 1;
+    } else if (s[i]=== '+'){
+        sign = 1;
+    } else if (s[i]=== '-'){
+        sign = -1;
+    } else if (s[i]=== '('){
+        // 현재까지 결과를 스택에 저장하고 현재 계산을 초기화
+        stack.push(sum);
+        stack.push(sign);
+        sum = 0;
+        sign = 1;
+    } else if (s[i]=== ')'){
+        sum = stack.pop() * sum;
+        sum += stack.pop();
+    }}
+    
+
+
+    return sum;
 };
